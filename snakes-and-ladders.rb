05453b9a101e22a5b79adc0b98fc553edcbdcaf5
mantonioisc <mantonioisc@outlook.com>
2014-04-01 10:35:04 -0700

    Dice and Player implemented

--- snakes-and-ladders.rb
+++ snakes-and-ladders.rb
@@ -4,6 +4,8 @@
 # Now I want to see how Ruby fares with a simple game like this one.
 #
 
+CHIP_COLORS = [:blue, :red, :green, :white, :purple]
+
 class Snake
 	attr_accessor :from, :to
 
@@ -31,16 +33,37 @@ class Ladder
 end
 
 class Dice
+	def initialize
+		@random = Random.new
+	end
 
+	def throw()
+		@random.rand(1..6)
+	end
+
+	def throwTwice()
+		@random.rand(1..12)
+	end
 end
 
-class Board
+DICE = Dice.new
 
+class Board
+	attr_accessor :snakes, :ladders
 end
 
 class Player
-	attr_accessor :name, :dice
+	attr_accessor :name, :chip, :dice
 
+	def initialize(name, chip, dice = DICE)
+		@name, @chip, @dice = name, chip, dice
+	end
+
+	def takeTurn()
+		advance_positions = @dice.throw
+		puts "Player #{@name}:#{@chip} advances #{advance_positions}"
+		advance_positions
+	end
 end
 
 class Game
@@ -50,6 +73,12 @@ end
 
 snake = Snake.new 10, 5
 ladder = Ladder.new 15, 20
+dice = Dice.new
+player = Player.new "Solid Snake", :blue
 
 puts snake
 puts ladder
+puts dice.throw
+puts dice.throwTwice
+puts DICE.throw
+player.takeTurn
